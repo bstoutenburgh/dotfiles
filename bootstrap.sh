186a95edfcbac1deca69adc6a83c42233c67f5a1
Ben Stoutenburgh <maristgeek@gmail.com>
2021-08-08 12:08:22 -0400

    fix some bootstrap logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,19 +4,22 @@
 
 set -e
 
+# a role will sort this out eventually but useful for this bootstrapping
+[[ ":$PATH:" != *"${HOME}/.local/bin"* ]] && PATH="${HOME}/.local/bin:${PATH}"
+
 if ! command -v ansible >/dev/null 2>&1; then
   sudo -v
   sudo apt install -y python3-venv
   pip install --user pipx
   pipx install --include-deps ansible
   pipx inject ansible argcomplete
-  # TODO: Add to PATH if not there?
 else
   pipx upgrade --include-injected ansible
+  pipx inject ansible argcomplete
 fi
 
 # clone the repo here
 
-ansible-galaxy collection install -r requirements-galaxy.yml
+ansible-galaxy install -r requirements-galaxy.yml
 
 # ansible-playbook --ask-become-pass desktop.yml
